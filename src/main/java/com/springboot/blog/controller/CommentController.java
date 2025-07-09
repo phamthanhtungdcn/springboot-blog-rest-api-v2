b6bd83cd95888c72465cabc28bfe75d275f99a6a
tungpt <phamthanhtungdcn@gmail.com>
2025-07-09 15:22:46 +0700

    Update Comment by id REST API

--- src/main/java/com/springboot/blog/controller/CommentController.java
+++ src/main/java/com/springboot/blog/controller/CommentController.java
@@ -33,4 +33,13 @@ public class CommentController {
                                                      @PathVariable(value = "id") Long commentId) {
         return  new ResponseEntity<>(commentService.getCommentById(postId, commentId), HttpStatus.OK);
     }
+
+    @PutMapping("/posts/{postId}/comments/{id}")
+    public ResponseEntity<CommentDto> updateComment(@PathVariable(value = "postId") Long postId,
+                                                    @PathVariable(value = "id") Long commentId,
+                                                    @RequestBody CommentDto commentDto){
+        CommentDto updatedComment = commentService.updateComment(postId, commentId, commentDto);
+
+        return new ResponseEntity<>(updatedComment, HttpStatus.OK);
+    }
 }
