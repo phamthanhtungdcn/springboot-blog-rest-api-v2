620352a0dbb0d73b1b3c6e123ea2b6b097b1f4ca
tungpt <phamthanhtungdcn@gmail.com>
2025-07-09 16:18:48 +0700

    Spring boot REST API Global exception handling

--- src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -28,6 +28,15 @@ public class GlobalExceptionHandler {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
                 webRequest.getDescription(false));
 
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception,
+                                                               WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
