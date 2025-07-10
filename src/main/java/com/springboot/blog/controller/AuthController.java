a3a9311f5fd26be2417048a485262a5e3eca49b7
tungpt <phamthanhtungdcn@gmail.com>
2025-07-10 17:48:16 +0700

    Create Register REST API

--- src/main/java/com/springboot/blog/controller/AuthController.java
+++ src/main/java/com/springboot/blog/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.springboot.blog.controller;
 
 import com.springboot.blog.payload.LoginDto;
+import com.springboot.blog.payload.RegisterDto;
 import com.springboot.blog.service.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,5 +24,12 @@ public class AuthController {
         String response = authService.login(loginDto);
 
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping(value = {"/register", "/signup"})
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto){
+        String response = authService.register(registerDto);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
